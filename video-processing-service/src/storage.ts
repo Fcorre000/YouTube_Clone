324ddc36cb617ef1d88b0e2421617275d0481ee6
Fernando Correa <fernandocorrea2014@gmail.com>
2025-06-24 19:52:15 -0500

    Updated docker file, started google cloud storage

---
+++ video-processing-service/src/storage.ts
@@ -0,0 +1,13 @@
+import {Storage} from '@google-cloud/storage';
+import fs from 'fs';
+import ffmpeg from 'fluent-ffmpeg';
+
+/**
+ * Creates the local directories for raw and processed videos.
+ */
+
+export function setupDirectories(){
+    /**
+     * @param rawVideoName - the name of the file to convert from {@link localRawVideoPath}
+     */
+}

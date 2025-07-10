215b8b8e9570e6c779493fe660d4b68ac9bb90f3
Fernando Correa <fernandocorrea2014@gmail.com>
2025-07-10 14:53:55 -0500

    Upload button complete, small changes to font and button size as well.

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css";
 import SignIn from "./sign-in";
+import Upload from "./upload";
 
 import { onAuthStateChangedHelper } from "../firebase/firebase";
 import {useEffect, useState} from "react";
@@ -23,11 +24,11 @@ export default function Navbar() {
     return (
         <nav className={styles.nav}>
             <Link href="/" >
-                 <Image width={90} height={20}
+                 <Image width={180} height={40}
                         src="/youtube-logo.svg" alt ="YouTube Logo"/>
             </Link>
             {
-                //ToDO: Add a upload
+                user && <Upload />
             }
             <SignIn user = {user} />
         </nav>
